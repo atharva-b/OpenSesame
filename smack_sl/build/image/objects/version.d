2bc3763e1e9e2f4d796be9bcf2e3d9f73e460729
Keval Tripathi <ktripath@uwaterloo.ca>
2025-02-28 22:20:57 -0500

    First Attempt at NVM

--- smack_sl/build/image/objects/version.d
+++ smack_sl/build/image/objects/version.d
@@ -1,4 +1,4 @@
-C:/InfineonSDK/smack_sl/build/image/objects/version.d C:/InfineonSDK/smack_sl/build/image/objects/version.o: \
- C:/InfineonSDK/smack_sl/src/version.c \
- C:/InfineonSDK/smack_sl/inc/version.h
-C:/InfineonSDK/smack_sl/inc/version.h:
+Z:/Documents/GitHub/OpenSesame/smack_sl/build/image/objects/version.d Z:/Documents/GitHub/OpenSesame/smack_sl/build/image/objects/version.o: \
+ Z:/Documents/GitHub/OpenSesame/smack_sl/src/version.c \
+ Z:/Documents/GitHub/OpenSesame/smack_sl/inc/version.h
+Z:/Documents/GitHub/OpenSesame/smack_sl/inc/version.h:
